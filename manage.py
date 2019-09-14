0897aed08e76e9b55673ce03a9173116d232896a
ddio <ddioibb@gmail.com>
2019-09-14 10:47:55 +0800

    Aviod module namespace conflict

--- manage.py
+++ manage.py
@@ -1,37 +1,13 @@
 import argparse
-import abc
-
-class SubCmd(abc.ABC):
-    @abc.abstractmethod
-    def add_parser(self, subparser):
-        return NotImplemented
-
-    @abc.abstractmethod
-    def execute(self, args):
-        return NotImplemented
-
-class ListCmd(SubCmd):
-    def add_parser(self, subparser):
-        self.parser = subparser.add_parser('list', help='List all existing crawler jobs')
-
-    def execute(self, args):
-        raise NotImplementedError()
-
-class CrawlCmd(SubCmd):
-    def add_parser(self, subparser):
-        self.parser = subparser.add_parser('crawl', help='Initiate a new crawler job')
-        self.parser.add_argument(
-            'url',
-            help='Search url of target category. Should be sth like https://rent.591.com.tw/home/search/rsList?is_new_list=1&type=1&kind=0&searchtype=1'
-        )
-
-    def execute(self, args):
-        raise NotImplementedError()
+from management import ListCmd, CrawlCmd, ResumeCmd, ExportCmd, DeleteCmd
 
 class EspressoCmd():
     cmds = dict(
         list=ListCmd(),
-        crawl=CrawlCmd()
+        crawl=CrawlCmd(),
+        resume=ResumeCmd(),
+        delete=DeleteCmd(),
+        export=ExportCmd()
     )
     def __init__(self):
         parser = argparse.ArgumentParser(description='Crawl and export 591 data once')
